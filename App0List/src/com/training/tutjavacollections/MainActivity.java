4d31daa650411a9e67bd3396fc4854686ae47526
ajinkyazore <25245619+ajinkyazore@users.noreply.github.com>
2018-09-01 13:50:03 +0530

    List Sample, Integer List Ambiguity

---
+++ App0List/src/com/training/tutjavacollections/MainActivity.java
@@ -0,0 +1,44 @@
+package com.training.tutjavacollections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.swing.event.ListSelectionEvent;
+
+public class MainActivity {
+ @SuppressWarnings("unchecked")
+public static void main(String[] args) {
+	
+	List<String> sampleStringList =Collections.synchronizedList(new ArrayList());	
+	
+	sampleStringList.add("Mary");
+	sampleStringList.add("Jane");
+	sampleStringList.add("Doe");
+	sampleStringList.remove(0);
+	
+	
+	sampleStringList.forEach(System.out::println);
+	
+	List<Integer> integerList = Collections.synchronizedList(new ArrayList());	
+	integerList.add(32);
+	integerList.add(34);
+	integerList.add(9);
+	
+	Integer intToAdd = new Integer(1000);
+	integerList.add(intToAdd);
+	
+	integerList.remove(0);
+	integerList.remove(new Integer(1000));
+	
+	
+	//predict the output here -> you will realize you're f**ked
+	integerList.forEach(System.out::println);
+	 
+}
+}
